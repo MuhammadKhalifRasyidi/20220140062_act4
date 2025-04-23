ebd7954d307ee1acdf06ae82c3b2827c6c0af6c3
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-04-23 23:18:09 +0700

    membuat halaman DetailOrderPage sebagai StatelessWidget

---
+++ lib/detail_order_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
+class DetailOrderPage extends StatelessWidget{
+  final String makanan;
+  final String minuman;
+  final String jumlahMakanan;
+  final String jumlahMinuman;
+  final int totalHarga;
+  const DetailOrderPage({
+    super.key,
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMakanan,
+    required this.jumlahMinuman,
+    required this.totalHarga,
+  });
+
+ 
+}
