bb87be4e287a08b4590fb5fc907ae2e1b016a551
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-04-23 22:57:41 +0700

    setup awal LoginPage menggunakan StatefulWidget

---
+++ lib/login_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/register_page.dart';
+
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
