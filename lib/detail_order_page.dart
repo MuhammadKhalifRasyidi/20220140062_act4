5786fb0252d94ca1ac853e616814249f1206ffc8
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-04-23 23:19:20 +0700

    menampilkan detail pesanan makanan, minuman, dan total harga

--- lib/detail_order_page.dart
+++ lib/detail_order_page.dart
@@ -19,7 +19,22 @@ class DetailOrderPage extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Detail Order Page')),
-      
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Food Order: $makanan'),
+            Text('Drink Order: $minuman'),
+            Text('Food QTY Order: $jumlahMakanan'),
+            Text('Drink QTY Order: $jumlahMinuman'),
+            Text('Total Price: $totalHarga'),
+
+            const SizedBox(height: 20),
+            
+          ],
+        ),
+      ),
     );
   }
 }
