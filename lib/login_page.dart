0edbc3ddabf955f313e8c670c52e4e0520415b84
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-04-23 23:03:30 +0700

    menambahkan navigasi ke HomePage setelah login berhasil

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -51,7 +51,18 @@ class _LoginPageState extends State<LoginPage> {
                   return null;
                 },
               ),
-              
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  }
+                },
+                child: Text('Login'),
+              ),
+             
             ],
           ),
         ),
