a44fab9e68edc6ccd01e82d6d603c28f67101dab
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-04-23 23:20:23 +0700

    inisialisasi root app Flutter dan kelas MyApp

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/login_page.dart';
+import 'package:flutter_application_1/order_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+ 
+}
