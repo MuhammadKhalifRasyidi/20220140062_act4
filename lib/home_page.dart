a2b4aba7c973578dcf0d8c04619d0edadd834322
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-04-23 23:09:13 +0700

    setup awal HomePage menggunakan StatefulWidget

---
+++ lib/home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/order_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
