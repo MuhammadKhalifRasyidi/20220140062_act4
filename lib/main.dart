e007952ed7ef3b5f1fba22e7f281ce7be2809b4b
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-04-23 23:20:52 +0700

    mengatur tema aplikasi menggunakan seedColor deepPurple

--- lib/main.dart
+++ lib/main.dart
@@ -10,5 +10,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
- 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      
+    );
+  }
 }
